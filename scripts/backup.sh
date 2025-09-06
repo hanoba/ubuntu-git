682262cb916c88c225843586ecab53fec36b8c63
Harald Bauer <harald@hanoba.de>
2025-09-06 11:38:21 +0200

    New version for bavkup on garten-pi HDD

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Daily backup mit tar
-#TARFILE=/tmp/ubuntu4-backup.tar.7z
 TARFILE=/tmp/ubuntu4-backup.tar
 BACKUPDIR=/home/harald/backup
 ARCHIVE=/root/Dropbox/1-Data.7z
 LOGFILE=/home/harald/logfile.txt
-#ARCHIVEDIR=/home/harald/1-Data
-#OUTLOOK=/home/harald/1-Data/.sync/Archive/0-Outlook
 ARCHIVEDIR=/media/DriveX/hba/1-Data   
 OUTLOOKDIR=/media/DriveX/hba/Mail/0-Outlook
 
@@ -38,34 +35,16 @@ tar -cf $TARFILE  --exclude=/root/Dropbox \
    --exclude=/home/harald/Downloads \
    --exclude=/home/harald/rompr \
    /etc /var/www /home /opt/fhem /media/DriveX/hba/www   >>$LOGFILE
-#tar -cf -  --exclude=/root/Dropbox \
-#   --exclude=".*" \
-#   --exclude=$ARCHIVEDIR \
-#   --exclude=/home/harald/artwork \
-#   --exclude=/home/harald/Downloads \
-#   --exclude=/home/harald/rompr \
-#   /etc /var/www /home /opt/fhem /media/DriveX/hba/www | 7z a -mhe=on -p2bYFGvE28m0NcUkTK8SW -si $TARFILE  >>$LOGFILE
-#mv $TARFILE /root/Dropbox
 
-# Create archive of 1-Data directory in Dropbox folder
+# Create archive of 1-Data directory in Dropbox folder 
+# (Dropbox folder is used for historic reasons. Dropbox is no longer used)
 rm -f $ARCHIVE
-#if [ -d $OUTLOOK ]; then rm -rf $OUTLOOK; fi
 7z a -mhe=on -xr!.sync -xr!.git -p2bYFGvE28m0NcUkTK8SW $ARCHIVE $ARCHIVEDIR $OUTLOOKDIR $TARFILE >>$LOGFILE
 date >>$LOGFILE
-# #
-# # Start Dropbox and wait until synchronization is complete
-# date >>$LOGFILE
-# #echo -e "$(date): Starting dropbox\n" >>$LOGFILE
-# dropbox start >>$LOGFILE
-# while [[ $(dropbox status) != Aktualisiert ]]; do sleep 20; done
-# #echo -e "$(date): Dropbox update completed\n" >>$LOGFILE
-# dropbox status >>$LOGFILE
-# #echo -e "$(date): Stopping dropbox\n" >>$LOGFILE
-# dropbox stop >>$LOGFILE
 
 # copy to harald-bauer.com
-sshpass -p 'HOny3gAdPcVOdm1JuK6p.' sftp  harald-bauer.com@ssh.harald-bauer.com >>$LOGFILE  <<END_SCRIPT
-cd backups
+sshpass -f /root/pw.txt sftp  harald@192.168.20.27 >>$LOGFILE  <<END_SCRIPT
+cd hdd/backups
 put $ARCHIVE
 rm     1-Data-7.7z
 rename 1-Data-6.7z 1-Data-7.7z
