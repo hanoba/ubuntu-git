177e8804a063ce2cbf0e1352adf63904cea20f8f
Harald Bauer <harald.n.bauer@web.de>
2025-11-04 08:00:34 +0100

    fhem.cfg and alexa-fhem.cfg added

---
+++ alexa-fhem/cp-all.sh
@@ -0,0 +1,6 @@
+# kopiert wichtige Dateien ins git
+sudo cp -a /home/harald/fhem-docker/alexa-fhem-ssh/. ./alexa-fhem-ssh/
+sudo chown -R harald:harald alexa-fhem-ssh/
+cp /home/harald/fhem-docker/alexa-fhem/alexa-fhem.cfg .
+cp /home/harald/fhem-docker/fhem-data/fhem.cfg .
+
--- alexa-fhem/cp-ssh.sh
+++
@@ -1,2 +0,0 @@
-sudo cp -a /home/harald/fhem-docker/alexa-fhem-ssh/. ./alexa-fhem-ssh/
-sudo chown -R harald:harald alexa-fhem-ssh/
